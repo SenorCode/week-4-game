28c99094bf1a4f3e1fe82a3708785c298a403875
SenorCode <appmar23@gmail.com>
2018-02-27 15:50:53 -0500

    fixed js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,80 +1,147 @@
-var numberToMatch = null;
-var wins = 0;
-var losses = 0;
-var score= null;
-var tmp = 0;
+$(document).ready(function() {
+
+	var numberToMatch = null;
+	var wins = 0;
+	var losses = 0;
+	var score= 0;
+
+	var crystal_one = null;
+	var crystal_two = null;
+	var crystal_three = null;
+	var crystal_four = null;
 
 
-var crystal_one = null;
-var crystal_two = null;
-var crystal_three = null;
-var crystal_four = null;
+	var updateNumberToMatch = function() {
+		numberToMatch = Math.floor(Math.random() * 101) + 19;
+		document.querySelector("#random").innerHTML = numberToMatch;  
+	};
+
+	var updateScore = function() {
+		document.querySelector("#score").innerHTML = score;
+		
+	};
 
 
-var updateNumberToMatch = function() {
-	numberToMatch = Math.floor(Math.random() * 120) + 19;
-	document.querySelector("#random").innerHTML = numberToMatch;  
-};
+	var updateCrystal_one = function() {
+		crystal_one = Math.floor(Math.random() * 11) + 1;
+		
+	};
+	var updateCrystal_two = function() {
+		crystal_two = Math.floor(Math.random() * 11) + 1;
+		
+	};
 
-var updateScore = function() {
-	
-	var score = tmp;
-	tmp
-	document.querySelector("#score").innerHTML = score;
-};
+	var updateCrystal_three = function() {
+		crystal_three = Math.floor(Math.random() * 11) + 1;
+		
+	};
 
-var updateCrystal_one = function() {
-	crystal_one = Math.floor(Math.random() * 12) + 1;
-	
-};
-var updateCrystal_two = function() {
-	crystal_two = Math.floor(Math.random() * 12) + 1;
-	
-};
+	var updateCrystal_four = function() {
+		crystal_four = Math.floor(Math.random() * 11) + 1;
+		
+	};
 
-var updateCrystal_three = function() {
-	crystal_three = Math.floor(Math.random() * 12) + 1;
-	
-};
+	var reset = function() {
+		updateNumberToMatch();
+		score = 0;
+		$("#score").text(score);
+		updateCrystal_one();
+		updateCrystal_two();
+		updateCrystal_three();
+		updateCrystal_four();
+	};
 
-var updateCrystal_four = function() {
-	crystal_four = Math.floor(Math.random() * 12) + 1;
-	
-};
-var reset = function() {
+
+	//excecute on page load
 	updateNumberToMatch();
 	updateScore();
 	updateCrystal_one();
 	updateCrystal_two();
 	updateCrystal_three();
 	updateCrystal_four();
-};
 
 
-//excecute on page load
-updateNumberToMatch();
-updateScore();
-updateCrystal_one();
-updateCrystal_two();
-updateCrystal_three();
-updateCrystal_four();
-console.log(crystal_one);
-$("#crystal_one").click(function(){
+	$("#crystal_one").click(function(){
 
-	score = crystal_one;
-	updateScore();
-	
+		score = score + crystal_one;
 
-});
+		updateScore();
 
-$("#crystal_two").on("click", function(event){
+		if(score == numberToMatch){
+			alert("You Won!");
+			wins++;
+			$("#wins").text(wins);
+			reset();
+		}
+		else if(score > numberToMatch){
+				alert("You Lose!");
+				losses++;
+				$("#losses").text(losses);
+				reset();
+		}
 
-});
+	});
 
-$("#crystal_three").on("click", function(event){
+	$("#crystal_two").click(function(){
 
-});
+		score = score + crystal_two;
 
-$("#crystal_four").on("click", function(event){
+		updateScore();
+		
+		if(score == numberToMatch){
+			alert("You Won!");
+			wins++;
+			$("#wins").text(wins);
+			reset();
+		}
+		else if(score > numberToMatch){
+				alert("You Lose!");
+				losses++;
+				$("#losses").text(losses);
+				reset();
+		}
 
+	});
+
+	$("#crystal_three").click(function(){
+
+		score = score + crystal_three;
+
+		updateScore();
+		
+		if(score == numberToMatch){
+			alert("You Won!");
+			wins++;
+			$("#wins").text(wins);
+			reset();
+		}
+		else if(score > numberToMatch){
+				alert("You Lose!");
+				losses++;
+				$("#losses").text(losses);
+				reset();
+		}
+
+	});
+
+	$("#crystal_four").click(function(){
+
+		score = score + crystal_four;
+
+		updateScore();
+		
+		if(score == numberToMatch){
+			alert("You Won!");
+			wins++;
+			$("#wins").text(wins);
+			reset();
+		}
+		else if(score > numberToMatch){
+				alert("You Lose!");
+				losses++;
+				$("#losses").text(losses);
+				reset();
+		}
+
+	});
 });
